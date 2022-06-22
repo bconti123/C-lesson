729d32e5d567433238ab8090b68d77b439224010
bconti123 <bconti00@gmail.com>
2022-06-21 20:10:12 -0700

    New lesson

---
+++ variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int year;
+    year = 2019;
+    int score = 0;
+    score = 1234 * 9;
+    std::cout << "Score is " << score << "\n";
+    int tip = 0;
+  
+    std::cout << "Enter tip amount: ";
+    std::cin >> tip;
+
+    std::cout << "You paid " << tip << " dollars. \n";
+
+    
+}
